3bd2cdfcc132292216b16769c549910281b72d73
黎 芷妤 <zhiyu@chtsecurity.com>
2024-07-30 16:44:56 +0800

    remove hamburger

--- script.js
+++ script.js
@@ -2,6 +2,5 @@ const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 
 hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navLinks.classList.toggle("active");
+    navLinks.classList.toggle("active");
 });
