ba4764f491478ab6b233463e937c39fe2f271174
黎 芷妤 <zhiyu@chtsecurity.com>
2024-07-30 16:21:38 +0800

    initial set

---
+++ main.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("active");
+});
